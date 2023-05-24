8f5d70ea2a5c0e6e396c921c743292eb83c51e76
elenejavakhishvili638 <elenejavaxishvili@yahoo.com>
2023-05-24 08:36:22 -0700

    style: landing page structure

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,29 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <div class="w-screen" >
+    <router-view v-slot="slotProps">
+      <transition name="route" mode="out-in">
+          <component :is="slotProps.Component"></component>
+        </transition>
+    </router-view>
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
+.route-enter-from {
+  opacity: 0;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+.route-leave-to {
+  opacity: 0;
 }
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
+.route-enter-active {
+  transition: all 0.2s ease-out;
 }
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
+.route-leave-active {
+  transition: all 0.2s ease-in;
 }
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+.route-enter-to,
+.route-leave-from {
+  opacity: 1;
 }
 </style>
