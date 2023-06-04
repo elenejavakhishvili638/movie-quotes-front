3d30720a83b3664ae06f62f813f08f9838415108
elenejavakhishvili638 <elenejavaxishvili@yahoo.com>
2023-06-04 10:43:19 -0700

    style: news feed header

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -59,11 +59,7 @@ export default {
       language,
       changeLanguage,
       showLang,
-      toggleLang
-    }
-  },
-  data() {
-    return {
+      toggleLang,
       Vector
     }
   },
--- src/views/NewsFeed.vue
+++ src/views/NewsFeed.vue
@@ -1,14 +1,17 @@
 <template>
-  <div>
-    <p>news feed</p>
-    <button @click="logout">logout</button>
+  <div class="background min-h-screen pb-[32px]">
+    <!-- <p>news feed</p> -->
+    <!-- <button @click="logout">logout</button> -->
+    <feed-header :logout="logout" :searchBar="true"></feed-header>
   </div>
 </template>
 
 <script>
+import FeedHeader from '../components/FeedHeader.vue'
 import { useLoginStore } from '../stores/login/index'
 
 export default {
+  components: { FeedHeader },
   setup() {
     const loginStore = useLoginStore()
 
@@ -26,3 +29,9 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+.background {
+  background: linear-gradient(187.16deg, #181623 0.07%, #191725 51.65%, #0d0b14 98.75%);
+}
+</style>
