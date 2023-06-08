21837556366c95e41a2a426e89736f8cb8c0c75d
elenejavakhishvili638 <elenejavaxishvili@yahoo.com>
2023-06-08 05:27:23 -0700

    refactor: fetch movies functionality

--- src/stores/movies/actions.js
+++ src/stores/movies/actions.js
@@ -3,9 +3,11 @@ import { fetchMovies, fetchMovie } from '../../services'
 export default {
   async fetchMovies(searchTerm) {
     try {
+      if (this.movieList.length > 0 && !searchTerm) {
+        return
+      }
       const response = await fetchMovies(searchTerm)
       this.movieList = response.data
-      console.log(this.movieList)
     } catch (error) {
       console.error(error)
     }
@@ -14,14 +16,11 @@ export default {
   async fetchMovie(id) {
     try {
       const movieFromList = this.movieList.find((movie) => movie.id === id)
-      console.log(movieFromList)
       if (movieFromList) {
         this.movie = movieFromList
-        console.log(this.movie)
       } else {
         const response = await fetchMovie(id)
         this.movie = response.data
-        console.log(this.movie)
       }
     } catch (error) {
       console.error(error)
