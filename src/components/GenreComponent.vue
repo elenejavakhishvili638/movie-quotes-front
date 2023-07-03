c98394a8dbe26625ed850e5d468859cbdff0b04f
elenejavakhishvili638 <elenejavaxishvili@yahoo.com>
2023-07-03 08:15:18 -0700

    style: movie inputs

--- src/components/GenreComponent.vue
+++ src/components/GenreComponent.vue
@@ -31,16 +31,16 @@ const genreRule = computed(() => {
   <div class="relative">
     <div
       @click="openGenreModal"
-      class="flex gap-1 w-full border border-[#6C757D] h-12 rounded items-center"
+      class="flex gap-1 w-full border border-[#6C757D] h-12 rounded items-center overflow-x-scroll"
     >
       <p class="ml-1 cursor-pointer" v-if="tagGenres.length === 0">Genres:</p>
       <div
-        class="text-white text-sm ml-1 bg-[#6C757D] py-0.25 px-0.5 rounded"
+        class="text-white text-xs ml-1 bg-[#6C757D] py-0.25 px-0.25 rounded min-w-[5rem] flex items-center justify-center"
         v-for="(tag, index) in tagGenres"
         :key="index"
       >
         {{ tag.name }}
-        <span @click="props.remove(tag.id)" class="ml-0.5">x</span>
+        <span @click="props.remove(tag.id)" class="ml-0.25 cursor-pointer text-xs">x</span>
       </div>
     </div>
     <Field name="genre" v-slot="{ handleChange }" :rules="genreRule">
@@ -50,7 +50,7 @@ const genreRule = computed(() => {
       >
         <span @click="closeGenreModal" class="bg-[#728ba1] text-center cursor-pointer">X</span>
         <div
-          class="text-white text-base border-b-2 py-0.25 px-0.5 rounded"
+          class="text-white text-base border-b-2 py-0.25 px-0.5 rounded cursor-pointer"
           v-for="(tag, index) in genres"
           :key="index"
           @click="handleClick(tag.name, handleChange)"
